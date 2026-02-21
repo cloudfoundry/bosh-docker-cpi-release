94ea808c29e85fb68c2689fa905006779fe936ed
aram price <aram.price@broadcom.com>
2026-02-20 17:05:56 -0800

    Fix shellcheck issues in tests/run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -1,22 +1,26 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
-set -e # -x
+if [[ -n "${DEBUG:-}" ]]; then
+  set -x
+  export BOSH_LOG_LEVEL=debug
+fi
 
 cpi_path=$PWD/cpi
 
 rm -f creds.yml
 
-echo "-----> `date`: Create dev release"
-bosh create-release --force --dir ./../ --tarball $cpi_path
+echo "-----> $(date): Create dev release"
+bosh create-release --force --dir ./../ --tarball "${cpi_path}"
 
-echo "-----> `date`: Create env"
+echo "-----> $(date): Create env"
 bosh create-env ~/workspace/bosh-deployment/bosh.yml \
   -o ~/workspace/bosh-deployment/docker/cpi.yml \
   -o ~/workspace/bosh-deployment/jumpbox-user.yml \
   -o ../manifests/dev.yml \
   --state=state.json \
   --vars-store=creds.yml \
-  -v docker_cpi_path=$cpi_path \
+  -v docker_cpi_path="${cpi_path}" \
   -v director_name=docker \
   -v internal_cidr=10.245.0.0/16 \
   -v internal_gw=10.245.0.1 \
@@ -28,27 +32,29 @@ bosh create-env ~/workspace/bosh-deployment/bosh.yml \
   -v network=net3
 
 export BOSH_ENVIRONMENT=10.245.0.11
-export BOSH_CA_CERT="$(bosh int creds.yml --path /director_ssl/ca)"
+BOSH_CA_CERT="$(bosh int creds.yml --path /director_ssl/ca)"
+export BOSH_CA_CERT
 export BOSH_CLIENT=admin
-export BOSH_CLIENT_SECRET="$(bosh int creds.yml --path /admin_password)"
+BOSH_CLIENT_SECRET="$(bosh int creds.yml --path /admin_password)"
+export BOSH_CLIENT_SECRET
 
-echo "-----> `date`: Update cloud config"
+echo "-----> $(date): Update cloud config"
 bosh -n update-cloud-config ~/workspace/bosh-deployment/docker/cloud-config.yml \
   -o reserved_ips.yml \
   -v network=net3
 
-echo "-----> `date`: Upload stemcell"
+echo "-----> $(date): Upload stemcell"
 bosh -n upload-stemcell "https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent?v=3445.11" \
   --sha1 d57c48cee58c71dce3707ff117ce79d01cc322ab
 
-echo "-----> `date`: Create env second time to test persistent disk attachment"
+echo "-----> $(date): Create env second time to test persistent disk attachment"
 bosh create-env ~/workspace/bosh-deployment/bosh.yml \
   -o ~/workspace/bosh-deployment/docker/cpi.yml \
   -o ~/workspace/bosh-deployment/jumpbox-user.yml \
   -o ../manifests/dev.yml \
   --state=state.json \
   --vars-store=creds.yml \
-  -v docker_cpi_path=$cpi_path \
+  -v docker_cpi_path="${cpi_path}" \
   -v director_name=docker \
   -v internal_cidr=10.245.0.0/16 \
   -v internal_gw=10.245.0.1 \
@@ -60,44 +66,44 @@ bosh create-env ~/workspace/bosh-deployment/bosh.yml \
   -v network=net3 \
   --recreate
 
-echo "-----> `date`: Delete previous deployment"
+echo "-----> $(date): Delete previous deployment"
 bosh -n -d zookeeper delete-deployment --force
 
-echo "-----> `date`: Deploy"
+echo "-----> $(date): Deploy"
 bosh -n -d zookeeper deploy <(wget -O- https://raw.githubusercontent.com/cppforlife/zookeeper-release/master/manifests/zookeeper.yml)
 
-echo "-----> `date`: Recreate all VMs"
+echo "-----> $(date): Recreate all VMs"
 bosh -n -d zookeeper recreate
 
-echo "-----> `date`: Exercise deployment"
+echo "-----> $(date): Exercise deployment"
 bosh -n -d zookeeper run-errand smoke-tests
 
-echo "-----> `date`: Restart deployment"
+echo "-----> $(date): Restart deployment"
 bosh -n -d zookeeper restart
 
-echo "-----> `date`: Report any problems"
+echo "-----> $(date): Report any problems"
 bosh -n -d zookeeper cck --report
 
-echo "-----> `date`: Delete random VM"
-bosh -n -d zookeeper delete-vm `bosh -d zookeeper vms|sort|cut -f5|head -1`
+echo "-----> $(date): Delete random VM"
+bosh -n -d zookeeper delete-vm "$(bosh -d zookeeper vms|sort|cut -f5|head -1)"
 
-echo "-----> `date`: Fix deleted VM"
+echo "-----> $(date): Fix deleted VM"
 bosh -n -d zookeeper cck --auto
 
-echo "-----> `date`: Delete deployment"
+echo "-----> $(date): Delete deployment"
 bosh -n -d zookeeper delete-deployment
 
-echo "-----> `date`: Clean up disks, etc."
+echo "-----> $(date): Clean up disks, etc."
 bosh -n -d zookeeper clean-up --all
 
-echo "-----> `date`: Deleting env"
+echo "-----> $(date): Deleting env"
 bosh delete-env ~/workspace/bosh-deployment/bosh.yml \
   -o ~/workspace/bosh-deployment/docker/cpi.yml \
   -o ~/workspace/bosh-deployment/jumpbox-user.yml \
   -o ../manifests/dev.yml \
   --state=state.json \
   --vars-store=creds.yml \
-  -v docker_cpi_path=$cpi_path \
+  -v docker_cpi_path="${cpi_path}" \
   -v director_name=docker \
   -v internal_cidr=10.245.0.0/16 \
   -v internal_gw=10.245.0.1 \
@@ -106,4 +112,4 @@ bosh delete-env ~/workspace/bosh-deployment/bosh.yml \
   -l docker-creds.yml \
   -v network=net3
 
-echo "-----> `date`: Done"
+echo "-----> $(date): Done"
