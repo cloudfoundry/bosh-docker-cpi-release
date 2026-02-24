dc2065d665a434aa12048c8ae51e32e4aa383e24
aram price <aram.price@broadcom.com>
2026-02-23 16:52:20 -0800

    CI: copy over cgroupsv2 / noble changes from bosh
    Co-authored-by: Brian Upton <brian.upton@broadcom.com>

--- ci/tasks/setup-director.sh
+++ ci/tasks/setup-director.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 set -e
+if [[ -n "${DEBUG:-}" ]]; then
+  set -x
+  export BOSH_LOG_LEVEL=debug
+fi
 
-
-cd ${PWD}/bosh-docker-cpi-release/
+cd "${PWD}/bosh-docker-cpi-release/"
 
 cpi_path=$(realpath ../bosh-cpi-dev-artifacts/release.tgz)
 
@@ -44,9 +47,9 @@ EOF
    bosh int ./certs.yml --path=/client_docker_tls/private_key > ./key.pem
     # generate certs in json format
     #
-   ruby -e 'puts File.read("./ca.pem").split("\n").join("\\n")' > $certs_dir/ca_json_safe.pem
-   ruby -e 'puts File.read("./cert.pem").split("\n").join("\\n")' > $certs_dir/client_certificate_json_safe.pem
-   ruby -e 'puts File.read("./key.pem").split("\n").join("\\n")' > $certs_dir/client_private_key_json_safe.pem
+   ruby -e 'puts File.read("./ca.pem").split("\n").join("\\n")' > "${certs_dir}/ca_json_safe.pem"
+   ruby -e 'puts File.read("./cert.pem").split("\n").join("\\n")' > "${certs_dir}/client_certificate_json_safe.pem"
+   ruby -e 'puts File.read("./key.pem").split("\n").join("\\n")' > "${certs_dir}/client_private_key_json_safe.pem"
   popd > /dev/null
 }
 
@@ -55,15 +58,28 @@ function sanitize_cgroups() {
   mountpoint -q /sys/fs/cgroup || \
     mount -t tmpfs -o uid=0,gid=0,mode=0755 cgroup /sys/fs/cgroup
 
+  if [ -f /sys/fs/cgroup/cgroup.controllers ]; then
+    # cgroups v2: enable nesting (based on moby/moby hack/dind)
+    mkdir -p /sys/fs/cgroup/init
+    # Loop to handle races from concurrent process creation (e.g. docker exec)
+    while ! {
+      xargs -rn1 < /sys/fs/cgroup/cgroup.procs > /sys/fs/cgroup/init/cgroup.procs 2>/dev/null || :
+      sed -e 's/ / +/g' -e 's/^/+/' < /sys/fs/cgroup/cgroup.controllers \
+        > /sys/fs/cgroup/cgroup.subtree_control
+    }; do true; done
+    return
+  fi
+
   mount -o remount,rw /sys/fs/cgroup
 
-  sed -e 1d /proc/cgroups | while read sys hierarchy num enabled; do
+  # shellcheck disable=SC2034
+  sed -e 1d /proc/cgroups | while read -r sys hierarchy num enabled; do
     if [ "$enabled" != "1" ]; then
       # subsystem disabled; skip
       continue
     fi
 
-    grouping="$(cat /proc/self/cgroup | cut -d: -f2 | grep "\\<$sys\\>")"
+    grouping="$(cut -d: -f2 < /proc/self/cgroup | grep "\\<$sys\\>")"
     if [ -z "$grouping" ]; then
       # subsystem not mounted anywhere; mount it on its own
       grouping="$sys"
@@ -95,19 +111,26 @@ function stop_docker() {
 }
 
 function start_docker() {
+  local certs_dir
+  certs_dir="${1}"
   # docker will fail starting with the new iptables. it throws:
   # iptables v1.8.7 (nf_tables): Could not fetch rule set generation id: ....
   update-alternatives --set iptables /usr/sbin/iptables-legacy
-  generate_certs $1
+  generate_certs "${certs_dir}"
   mkdir -p /var/log
   mkdir -p /var/run
 
   sanitize_cgroups
 
-  # ensure systemd cgroup is present
-  mkdir -p /sys/fs/cgroup/systemd
-  if ! mountpoint -q /sys/fs/cgroup/systemd ; then
-    mount -t cgroup -o none,name=systemd cgroup /sys/fs/cgroup/systemd
+  # systemd inside nested Docker containers requires shared mount propagation
+  mount --make-rshared /
+
+  # ensure systemd cgroup is present (cgroups v1 only)
+  if [ ! -f /sys/fs/cgroup/cgroup.controllers ]; then
+    mkdir -p /sys/fs/cgroup/systemd
+    if ! mountpoint -q /sys/fs/cgroup/systemd ; then
+      mount -t cgroup -o none,name=systemd cgroup /sys/fs/cgroup/systemd
+    fi
   fi
 
   # check for /proc/sys being mounted readonly, as systemd does
@@ -115,7 +138,8 @@ function start_docker() {
     mount -o remount,rw /proc/sys
   fi
 
-  local mtu=$(cat /sys/class/net/$(ip route get 8.8.8.8|awk '{ print $5 }')/mtu)
+  local mtu
+  mtu=$(cat "/sys/class/net/$(ip route get 8.8.8.8|awk '{ print $5 }')/mtu")
 
   [[ ! -d /etc/docker ]] && mkdir /etc/docker
   cat <<EOF > /etc/docker/daemon.json
@@ -140,7 +164,7 @@ EOF
 
   rc=1
   for i in $(seq 1 100); do
-    echo waiting for docker to come up...
+    echo "waiting for docker to come up... (${i})"
     sleep 1
     set +e
     docker info
@@ -155,13 +179,23 @@ EOF
     exit 1
   fi
 
-  echo $certs_dir
+  echo "${certs_dir}"
 }
 
 function main() {
-  export OUTER_CONTAINER_IP=$(ruby -rsocket -e 'puts Socket.ip_address_list
-                          .reject { |addr| !addr.ip? || addr.ipv4_loopback? || addr.ipv6? }
-                          .map { |addr| addr.ip_address }')
+  OUTER_CONTAINER_IP=$(
+    ip addr \
+    | grep 'inet ' \
+    | grep -v -E ' (127\.|172\.|10\.245)' \
+    | cut -d/ -f 1 \
+    | cut -d' ' -f6
+  )
+  export OUTER_CONTAINER_IP
+
+  if [[ "${OUTER_CONTAINER_IP}" == *$'\n'* ]] ; then
+    echo "OUTER_CONTAINER_IP had more than one ip: '${OUTER_CONTAINER_IP}'" >&2
+    exit 1
+  fi
 
   export DOCKER_HOST="tcp://${OUTER_CONTAINER_IP}:4243"
 
@@ -179,25 +213,25 @@ function main() {
 
   mkdir -p ${local_bosh_dir}
 
-  command bosh int ../bosh-deployment/bosh.yml \
+  bosh int ../bosh-deployment/bosh.yml \
     -o ../bosh-deployment/docker/cpi.yml \
     -o ../bosh-deployment/docker/use-jammy.yml \
     -o ../bosh-deployment/jumpbox-user.yml \
     -o ../bosh-deployment/misc/source-releases/bosh.yml \
     -o manifests/dev.yml \
     -v director_name=docker \
-    -v docker_cpi_path=$cpi_path \
+    -v docker_cpi_path="${cpi_path}" \
     -v internal_cidr=10.245.0.0/16 \
     -v internal_gw=10.245.0.1 \
     -v internal_ip="${BOSH_DIRECTOR_IP}" \
     -v docker_host="${DOCKER_HOST}" \
     -v network=director_network \
-    -v docker_tls="{\"ca\": \"$(cat ${certs_dir}/ca_json_safe.pem)\",\"certificate\": \"$(cat ${certs_dir}/client_certificate_json_safe.pem)\",\"private_key\": \"$(cat ${certs_dir}/client_private_key_json_safe.pem)\"}" \
-    ${@} > "${local_bosh_dir}/bosh-director.yml"
+    -v docker_tls="{\"ca\": \"$(cat "${certs_dir}/ca_json_safe.pem")\",\"certificate\": \"$(cat "${certs_dir}/client_certificate_json_safe.pem")\",\"private_key\": \"$(cat "${certs_dir}/client_private_key_json_safe.pem")\"}" \
+    "${@}" > "${local_bosh_dir}/bosh-director.yml"
 
-  command bosh create-env "${local_bosh_dir}/bosh-director.yml" \
-          --vars-store="${local_bosh_dir}/creds.yml" \
-          --state="${local_bosh_dir}/state.json"
+  bosh create-env "${local_bosh_dir}/bosh-director.yml" \
+      --vars-store="${local_bosh_dir}/creds.yml" \
+      --state="${local_bosh_dir}/state.json"
 
   bosh int "${local_bosh_dir}/creds.yml" --path /director_ssl/ca > "${local_bosh_dir}/ca.crt"
   bosh -e "${BOSH_DIRECTOR_IP}" --ca-cert "${local_bosh_dir}/ca.crt" alias-env "${BOSH_ENVIRONMENT}"
@@ -205,7 +239,7 @@ function main() {
   cat <<EOF > "${local_bosh_dir}/env"
   export BOSH_ENVIRONMENT="${BOSH_ENVIRONMENT}"
   export BOSH_CLIENT=admin
-  export BOSH_CLIENT_SECRET=`bosh int "${local_bosh_dir}/creds.yml" --path /admin_password`
+  export BOSH_CLIENT_SECRET=$(bosh int "${local_bosh_dir}/creds.yml" --path /admin_password)
   export BOSH_CA_CERT="${local_bosh_dir}/ca.crt"
 
 EOF
@@ -215,4 +249,4 @@ EOF
 
 }
 
-main $@
+main "${@}"
