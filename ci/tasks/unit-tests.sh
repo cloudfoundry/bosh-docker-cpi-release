7144fe47754be6cb3180d920d0ab45464844c3e7
aram price <aram.price@broadcom.com>
2026-03-10 12:53:38 -0700

    CI: remove unused tasks; rename resources; use ghcr.io

--- ci/tasks/unit-tests.sh
+++ ci/tasks/unit-tests.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
+set -eu -o pipefail
 
-set -e
+REPO_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
+REPO_PARENT="$( cd "${REPO_ROOT}/.." && pwd )"
 
-source ci/ci/tasks/utils.sh
+if [[ -n "${DEBUG:-}" ]]; then
+  set -x
+  export BOSH_LOG_LEVEL=debug
+  export BOSH_LOG_PATH="${BOSH_LOG_PATH:-${REPO_PARENT}/bosh-debug.log}"
+fi
 
-check_go_version ${PWD}/bosh-docker-cpi-release
+cpi_package_go_version="$(
+  grep linux "${REPO_PARENT}"/bosh-docker-cpi-release/packages/golang-*/spec.lock \
+  | awk '{print $2}' \
+  | sed "s/golang-\(.*\)-linux/\1/"
+)"
 
-${PWD}/bosh-docker-cpi-release/scripts/test-unit
+current=$(go version)
+if [[ "$current" != *"${cpi_package_go_version}"* ]]; then
+  echo "Current go version '${current}' does not match CPI's packaged go version '${cpi_package_go_version}'"
+  exit 1
+fi
+
+"${REPO_PARENT}/bosh-docker-cpi-release/scripts/test-unit"
